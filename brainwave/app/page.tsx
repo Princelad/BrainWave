ddcda7e5efca2f9f4a4b5e113b97fb604132b057
shreylakhtaria <shrey.lakhataria.code@gmail.com>
2024-08-07 14:19:00 +0530

    enviroment_setup_done

---
+++ brainwave/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { DESTRUCTION } from "node:dns";
+import { describe } from "node:test";
+
+export default function Home() {
+  return(   
+
+    <Button variant={"destructive"}>Delete</Button>
+  );
+}
