3b6bf7178b58147a00fd55d05312921cb2d17eb9
shreylakhtaria <shrey.lakhataria.code@gmail.com>
2024-08-08 14:29:50 +0530

    added_logo

--- brainwave/app/page.tsx
+++ brainwave/app/page.tsx
@@ -1,11 +1,6 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { DESTRUCTION } from "node:dns";
-import { describe } from "node:test";
 
 export default function Home() {
-  return(   
-
-    <Button variant={"destructive"}>Delete</Button>
-  );
+  return <Button>
+    Click Me</Button>;
 }
---
+++ brainwave/app/test/page.tsx
@@ -0,0 +1,9 @@
+const test = () => {
+    return ( 
+        <div>
+        Hello 
+        </div>
+     );
+}
+ 
+export default test;
